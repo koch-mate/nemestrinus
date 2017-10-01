bfb7d70af1efb53aab37ff7ce69eb2f5c474ddd9
koch-mate <mkoch@Koch-Mate-2.local>
2017-10-01 22:09:27 +0200

    Fix commit

--- ajax/getDistance.php
+++ ajax/getDistance.php
@@ -5,6 +5,7 @@ header('Content-type: application/json');
 $url = 'https://maps.googleapis.com/maps/api/distancematrix/json';
 $key = 'AIzaSyCNgpxoeDSu7tMM5SoTo0d-Gh3JZHrrXAY';
 $o = $_POST['origins'];
+
 $d = urlencode($_POST['destinations']);
 
 $val =  file_get_contents("$url?origins=$o&destinations=$d&key=$key");
