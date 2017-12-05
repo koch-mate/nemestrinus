f1396c2dabc2d73d45b4aec46b5ba31404e4d25d
koch-mate <mkoch@Koch-Mate-2.local>
2017-12-05 22:36:18 +0100

    Fix: #71

--- pages/kimutatas-attekintes.php
+++ pages/kimutatas-attekintes.php
@@ -23,11 +23,18 @@ foreach([P_EURO, P_FORINT] as $p){ ?>
   <div class="col-md-4 clearfix" style="background:rgba(255, 255, 255, 0.9);margin:2em;padding:1em;">
 
   <h3>Lejárt határidejű, befizetetlen, teljesített megrendelések - <?=$p?></h3>
-  <p>
-    Minden adat a tárgyévben (<?=$ev?>) <i>rögzített</i> megrendelésekre vonatkozik.
-  </p>
 <div style="max-height:300px;overflow:auto;">
-
+<?php
+$d= $db->select('megrendeles', ['ID', 'Tipus', 'MegrendeloID', 'SzamlaSzam', 'MegrendeloNev', 'FizetesiHatarido', 'Vegosszeg', 'Penznem', 'FizetesiHatarido'], ['AND'=>['Deleted'=>0, 'Penznem'=>$p, 'Statusz'=>M_S_TELJESITVE, 'SzallitasStatusza'=>SZ_S_LESZALLITVA, 'FizetesStatusza'=>F_S_FIZETESRE_VAR, 'FizetesiHatarido[<]'=>date('Y-m-d')], 'ORDER'=>['FizetesiHatarido'=>'ASC']]);
+if(empty($d)){
+  ?>
+<p>
+<em>A nincs lejárt fizetésű, kiszállított rendelés.</em>
+</p>
+  <?php
+}
+  else{
+ ?>
   <table class="table">
     <thead>
       <tr>
@@ -41,12 +48,11 @@ foreach([P_EURO, P_FORINT] as $p){ ?>
     </thead>
     <tbody>
       <?php
-    $d= $db->select('megrendeles', ['ID', 'Tipus', 'MegrendeloID', 'SzamlaSzam', 'MegrendeloNev', 'FizetesiHatarido', 'Vegosszeg', 'Penznem', 'FizetesiHatarido'], ['AND'=>['Deleted'=>0, 'Penznem'=>$p, 'Statusz'=>M_S_TELJESITVE, 'SzallitasStatusza'=>SZ_S_LESZALLITVA, 'FizetesStatusza'=>F_S_FIZETESRE_VAR, 'FizetesiHatarido[<]'=>date('Y-m-d')], 'ORDER'=>['FizetesiHatarido'=>'ASC']]);
 
     foreach($d as $di){
 ?>
       <tr>
-        <th><a href="?mode=megrendeles-osszesites&id=<?=$di[ID]?>"><?=$di[ID]?></a></th>
+        <th><a href="?mode=megrendeles-osszesites&id=<?=$di['ID']?>"><?=$di['ID']?></a></th>
         <td><span class="glyphicon glyphicon-<?=($di['Tipus']==M_LAKOSSAGI ? 'home" title="lakossági':'globe" title="export')?>" aria-hidden="true"></span></td>
         <td><?=($di['Tipus']==M_LAKOSSAGI ? $di['MegrendeloNev'] : exportCustomerGetNameById($di['MegrendeloID']))?></td>
         <td style="text-align:right;"><?=$di['SzamlaSzam']?></td>
@@ -59,15 +65,20 @@ foreach([P_EURO, P_FORINT] as $p){ ?>
    ?>
  </tbody>
 </table>
+<?php } ?>
 </div>
+<p style="margin-top:2em;">
+  A tárgyévben (<?=$ev?>) <i>teljesített/teljesítendő</i> megrendelések:
+</p>
+
 
   <?php
 
-  $fh   = 0+$db->sum('megrendeles', 'Vegosszeg', Medoo::raw("WHERE (<Deleted> = 0 AND (<RogzitesDatum> BETWEEN '".$ev."-01-01' AND '".$ev."-12-31') AND <Penznem> = '".$p."' AND <FizetesStatusza> = '".F_S_FIZETVE."' AND <FizetesDatuma> <= <FizetesiHatarido>)"));
-  $fht  = 0+$db->sum('megrendeles', 'Vegosszeg', Medoo::raw("WHERE (<Deleted> = 0 AND (<RogzitesDatum> BETWEEN '".$ev."-01-01' AND '".$ev."-12-31') AND <Penznem> = '".$p."' AND <FizetesStatusza> = '".F_S_FIZETVE."' AND <FizetesDatuma> > <FizetesiHatarido>)"));
-  $nfh  = 0+$db->sum('megrendeles', 'Vegosszeg', Medoo::raw("WHERE (<Deleted> = 0 AND (<RogzitesDatum> BETWEEN '".$ev."-01-01' AND '".$ev."-12-31') AND <Penznem> = '".$p."' AND <FizetesStatusza> = '".F_S_FIZETESRE_VAR."' AND '".date('Y-m-d')."' <= <FizetesiHatarido>)"));
-  $nfht = 0+$db->sum('megrendeles', 'Vegosszeg', Medoo::raw("WHERE (<Deleted> = 0 AND <SzallitasStatusza> != '".SZ_S_LESZALLITVA."' AND(<RogzitesDatum> BETWEEN '".$ev."-01-01' AND '".$ev."-12-31') AND <Penznem> = '".$p."' AND <FizetesStatusza> = '".F_S_FIZETESRE_VAR."' AND '".date('Y-m-d')."' > <FizetesiHatarido>)"));
-  $nfhtk = 0+$db->sum('megrendeles', 'Vegosszeg', Medoo::raw("WHERE (<Deleted> = 0 AND <SzallitasStatusza> = '".SZ_S_LESZALLITVA."' AND (<RogzitesDatum> BETWEEN '".$ev."-01-01' AND '".$ev."-12-31') AND <Penznem> = '".$p."' AND <FizetesStatusza> = '".F_S_FIZETESRE_VAR."' AND '".date('Y-m-d')."' > <FizetesiHatarido>)"));
+  $fh   = 0+$db->sum('megrendeles', 'Vegosszeg', Medoo::raw("WHERE (<Deleted> = 0 AND (<KertDatum> BETWEEN '".$ev."-01-01' AND '".$ev."-12-31') AND <Penznem> = '".$p."' AND <FizetesStatusza> = '".F_S_FIZETVE."' AND <FizetesDatuma> <= <FizetesiHatarido>)"));
+  $fht  = 0+$db->sum('megrendeles', 'Vegosszeg', Medoo::raw("WHERE (<Deleted> = 0 AND (<KertDatum> BETWEEN '".$ev."-01-01' AND '".$ev."-12-31') AND <Penznem> = '".$p."' AND <FizetesStatusza> = '".F_S_FIZETVE."' AND <FizetesDatuma> > <FizetesiHatarido>)"));
+  $nfh  = 0+$db->sum('megrendeles', 'Vegosszeg', Medoo::raw("WHERE (<Deleted> = 0 AND (<KertDatum> BETWEEN '".$ev."-01-01' AND '".$ev."-12-31') AND <Penznem> = '".$p."' AND <FizetesStatusza> = '".F_S_FIZETESRE_VAR."' AND '".date('Y-m-d')."' <= <FizetesiHatarido>)"));
+  $nfht = 0+$db->sum('megrendeles', 'Vegosszeg', Medoo::raw("WHERE (<Deleted> = 0 AND <SzallitasStatusza> != '".SZ_S_LESZALLITVA."' AND(<KertDatum> BETWEEN '".$ev."-01-01' AND '".$ev."-12-31') AND <Penznem> = '".$p."' AND <FizetesStatusza> = '".F_S_FIZETESRE_VAR."' AND '".date('Y-m-d')."' > <FizetesiHatarido>)"));
+  $nfhtk = 0+$db->sum('megrendeles', 'Vegosszeg', Medoo::raw("WHERE (<Deleted> = 0 AND <SzallitasStatusza> = '".SZ_S_LESZALLITVA."' AND (<KertDatum> BETWEEN '".$ev."-01-01' AND '".$ev."-12-31') AND <Penznem> = '".$p."' AND <FizetesStatusza> = '".F_S_FIZETESRE_VAR."' AND '".date('Y-m-d')."' > <FizetesiHatarido>)"));
 
    ?>
 
@@ -75,28 +86,39 @@ foreach([P_EURO, P_FORINT] as $p){ ?>
 
 <table class="table">
   <tr>
+    <th>F+FHT</th>
+    <th>Teljes bevétel </th>
+    <th><span class="badge badge-pill " ><?=ezres($fh+$fht)?>&nbsp;<?=$p?></span></th>
+  </tr>
+  <tr>
     <td>F</td>
-    <td>Határidőig kifizetett megrendelések a tárgyévben: </td>
+    <td>Határidőig <?=($p==P_EURO?'kifizetett':'kiszállított')?> megrendelések a tárgyévben </td>
     <td><span class="badge badge-pill " style="background:rgb(2 , 124, 0);"><?=ezres($fh)?>&nbsp;<?=$p?></span></td>
   </tr>
   <tr>
     <td>FHT  </td>
-    <td>Határidőn túl kifizetett megrendelések a tárgyévben:  </td>
+    <td>Határidőn túl <?=($p==P_EURO?'kifizetett':'kiszállított')?> megrendelések a tárgyévben  </td>
     <td><span class="badge badge-pill "  style="background:rgb(125, 189, 48)"><?=ezres($fht)?>&nbsp;<?=$p?></span></td>
   </tr>
   <tr>
+    <th>NF+NFHT+NFHTK</th>
+    <th>Teljes kintlevőség </th>
+    <th><span class="badge badge-pill " ><?=ezres($nfh+$nfht+$nfhtk)?>&nbsp;<?=$p?></span></th>
+  </tr>
+
+  <tr>
     <td>NF  </td>
-    <td>Határidőn belüli, fizetésre váró megrendelések:  </td>
-    <td>  <span class="badge badge-pill "  style="background:rgb(120, 150, 19)"><?=ezres($nfh)?>&nbsp;<?=$p?></span></td>
+    <td>Határidőn belüli, <?=($p==P_EURO?'fizetésre':'szállításra')?> váró megrendelésel  </td>
+    <td>  <span class="badge badge-pill "  style="background:orange"><?=ezres($nfh)?>&nbsp;<?=$p?></span></td>
   </tr>
   <tr>
     <td>NFHT  </td>
-    <td>Lejárt határidejű, kifizetetlen, még nem kiszállított megrendelések:  </td>
+    <td>Lejárt határidejű,  <?=($p==P_EURO?'kifizetetetlen,':'')?> még nem kiszállított megrendelések  </td>
     <td>  <span class="badge badge-pill "  style="background:rgb(169, 3, 6)"><?=ezres($nfht)?>&nbsp;<?=$p?></span></td>
   </tr>
   <tr>
     <td>NFHTK  </td>
-    <td>Lejárt határidejű, kifizetetlen, kiszállított megrendelések:  </td>
+    <td>Lejárt határidejű, kifizetetlen, kiszállított megrendelések  </td>
     <td><span class="badge badge-pill "  style="background:rgb(249, 3, 6)"><?=ezres($nfhtk)?>&nbsp;<?=$p?></span></td>
   </tr>
 </table>
@@ -125,7 +147,7 @@ foreach([P_EURO, P_FORINT] as $p){ ?>
           },
         },
         "data": {
-          "labels" : ["F: fizetve határidőig","FHT: fizetve határidőn túl","NF: nincs fizetve, határidőn belül","NFHT: nincs fizetve, határidőn túl, nincs kiszállítva","NFHTK: nincs fizetve, határidőn túl, kiszállítva"],
+          "labels" : ["F: <?=($p==P_EURO?'fizetve':'szállítva')?> határidőig","FHT: <?=($p==P_EURO?'fizetve':'szállítva')?> határidőn túl","NF: nincs <?=($p==P_EURO?'fizetve':'szállítva')?>, határidőn belül","NFHT: nincs fizetve, határidőn túl, nincs kiszállítva","NFHTK: nincs fizetve, határidőn túl, kiszállítva"],
           "datasets" : [
             {
               "label" : "<?=$p?>",
@@ -133,7 +155,7 @@ foreach([P_EURO, P_FORINT] as $p){ ?>
               "backgroundColor" : [
                 "rgb(2 , 54, 0)",
                 "rgb(125, 189, 48)",
-                "rgb(120, 150, 19)",
+                "rgb(255, 165, 0)",
                 "rgb(169, 3, 6)",
                 "rgb(249, 3, 6)"
               ]
