601094e19ed75cca6fbb92389b18a6c685ad7f06
Mate Koch <koch.mate@me.com>
2021-02-13 22:10:45 +0100

    Limit log entry list to last 1k

--- lib/log.php
+++ lib/log.php
@@ -4,13 +4,13 @@
 function getEvents(){
     global $db;
     return $db->select('naplo', ['ID', 'UserID', 'Timestamp', 'Action', 'OldValue', 'NewValue']
-               ,["ORDER"=>['Timestamp'=>'DESC']]);
+               ,["ORDER"=>['Timestamp'=>'DESC'], "LIMIT"=>1000]);
 }
 
 function logEv($ev, $old=null, $new=null){
     global $db;
     $db->insert('naplo', [
-        'UserID'=>$_SESSION['userID'], 
+        'UserID'=>$_SESSION['userID'],
         'Timestamp' =>date('Y-m-d H:i:s'),
         'Action' =>$ev,
         'OldValue'=>$old,
